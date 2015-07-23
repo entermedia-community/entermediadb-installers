5c6850e39185f4f1f737f735d1d61bbbccad70f5
cburkey <cburkey@openedit.org>
2015-07-23 15:32:15 -0400

    Start

--- linux/usr/share/entermediadb/bin/startdropbox.sh
+++ linux/usr/share/entermediadb/bin/startdropbox.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 CLIENT_ROOT="/opt/entermediadb/clients/home";
 if [[ -z "$1" ]]; then
-	cd $CLIENT_ROOT;
-	CLIENTS=($(ls -d */));
+	##cd $CLIENT_ROOT;
+	CLIENTS="$CLIENT_ROOT/*";
 else
 	CLIENTS=($1)
 fi
+
+rm -f ~/.dropbox/dropbox.pid
+
 for CLIENT in $CLIENTS; do
-	CLIENT_DIR="$CLIENT_ROOT/$CLIENT"
-	echo "Starting dropbox client for $CLIENT"
-	HOME="$CLIENT_DIR"
-	${CLIENT_DIR}/.dropbox-dist/dropboxd &  
-	sleep 5
+	if [[ -d "$CLIENT/.dropbox/instance_db" ]]; then
+		echo "Starting dropbox client for $CLIENT"
+		HOME="$CLIENT"
+		rm -f "$CLIENT/.dropbox/dropbox.pid"
+		$CLIENT/.dropbox-dist/dropboxd >> /tmp/log.log 2>&1 & disown
+		sleep 5
+	fi
 done
+echo "Clients all started"
