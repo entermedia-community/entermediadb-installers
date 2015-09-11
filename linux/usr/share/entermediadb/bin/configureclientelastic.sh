51e42f9599b9269b9304c80d4713d55c743f1d78
cburkey <cburkey@openedit.org>
2015-09-11 17:14:20 -0400

    Clean up errors

--- linux/usr/share/entermediadb/bin/configureclientelastic.sh
+++ linux/usr/share/entermediadb/bin/configureclientelastic.sh
@@ -7,6 +7,7 @@ CLIENT_NAME=$3
 WEBAPP="/opt/entermediadb/webapp"
 CATALOG="$WEBAPP/WEB-INF/data/$CLIENT_ID/catalog"
 
+mkdir $WEBAPP/$CLIENT_ID/
 cp -rp /opt/entermediadb/webapp/learningmedia/*  $WEBAPP/$CLIENT_ID/
 
 echo 'Copying system users/groups'
@@ -16,11 +17,11 @@ mkdir -p $CATALOG/groups
 cp -rp $WEBAPP/WEB-INF/data/system/groups/*.xml $CATALOG/groups
 echo 'Linking base.xml'
 mkdir -p $WEBAPP/$CLIENT_ID/catalog
-ln -s /usr/share/entermediadb/common/catalog/configuration $WEBAPP/$CLIENT_ID/catalog/configuration
+ln -s /usr/share/entermediadb/common/catalog/configuration $WEBAPP/$CLIENT_ID/catalog/
 
 mkdir -p $CATALOG/lists/searchtypes/
 
-sed "s:%client_id:$CLIENT_ID:g;s:%client_name:$CLIENT_NAME:g;s:%sub_domain:$SUB_DOMAIN:g" < /usr/share/entermediadb/common/catalog/catalog/searchtypes/custom.xml >$CATALOG/lists/searchtypes/custom.xml
+sed "s:%client_id:$CLIENT_ID:g;s:%client_name:$CLIENT_NAME:g;s:%sub_domain:$SUB_DOMAIN:g" < /usr/share/entermediadb/common/catalog/searchtypes/custom.xml >$CATALOG/lists/searchtypes/custom.xml
 
 mkdir -p $CATALOG/lists/catalogsettings/
 
@@ -37,6 +38,6 @@ echo -e $outfile > /opt/entermediadb/webapp/$CLIENT_ID/emshare/_site.xconf
 outfile="<page> <property name=\"fallbackdirectory\">/WEB-INF/base/mediadb</property> <property name=\"title\">$CLIENT_NAME - EMShare</property> <property name=\"appmodule\">asset</property> <property name=\"themeid\">emsharedefault</property> <property name=\"catalogid\">$CLIENT_ID/catalog</property> <property name=\"applicationid\">$CLIENT_ID/emshare</property></page>"
 echo -e $outfile > /opt/entermediadb/webapp/$CLIENT_ID/mediadb/_site.xconf
 
-outfile=\<page><property name=\"fallbackdirectory\">/media/catalog</property><property name=\"catalogid\">$CLIENT_ID/catalog</property></page>"
+outfile="<page><property name=\"fallbackdirectory\">/media/catalog</property><property name=\"catalogid\">$CLIENT_ID/catalog</property></page>"
 echo -e $outfile > /opt/entermediadb/webapp/$CLIENT_ID/catalog/_site.xconf
 
--- linux/usr/share/entermediadb/bin/exportwelcome.sh
+++ linux/usr/share/entermediadb/bin/exportwelcome.sh
@@ -1,9 +1,9 @@
 CLIENT_DOMAIN=$1
 CLIENT_ID=$2
 CLIENT_NAME=$3
-sed "s:%client_name%:$CLIENT_NAME:g;s:%client_id%:$CLIENT_ID:g;s:%client_domain:$CLIENT_DOMAIN:g" </opt/entermediadb/common/client_template/index.html.client >/opt/entermediadb/webapp/$CLIENT_ID/index.html
+#sed "s:%client_name%:$CLIENT_NAME:g;s:%client_id%:$CLIENT_ID:g;s:%client_domain:$CLIENT_DOMAIN:g" </usr/share/entermediadb/common/client_template/index.html.client >/opt/entermediadb/webapp/$CLIENT_ID/index.html
 #cp -r /opt/entermediadb/webapp/theme/index_files /opt/entermediadb/webapp/$WEB_FOLDER/
-echo "Exported index.html for $CLIENT_NAME, id: $CLIENT_ID"
+#echo "Exported index.html for $CLIENT_NAME, id: $CLIENT_ID"
 outfile="<page>\n   <inner-layout>/theme/layouts/landing.html</inner-layout>\n  <property name=\"clientid\">$CLIENT_ID</property>\n <property name=\"clientname\">$CLIENT_NAME</property>\n <property name=\"clientdomainprefix\">$CLIENT_DOMAIN</property>\n</page>"
 echo -e $outfile > /opt/entermediadb/webapp/$CLIENT_ID/index.xconf
 echo Exported xconf for $CLIENT_NAME
