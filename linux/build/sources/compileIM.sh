c17c39972f6ec542ac6ff796e956bfa961ae1883
ec2-user <ec2-user@ip-172-31-50-226.ec2.internal>
2015-10-28 17:38:37 -0400

    IM package built with GS 8.7

--- linux/build/sources/compileIM.sh
+++ linux/build/sources/compileIM.sh
@@ -9,23 +9,24 @@ if [ ! -f "~/.rpmmacros" ]; then
 fi
 
 
-yum install -y  rpmdevtools yasm wget cmake gcc gcc-c++
+#sudo yum install -y  rpmdevtools yasm wget cmake gcc gcc-c++
 
-wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-sudo rpm -Uvh epel-release-6*.rpm
+#wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+#sudo rpm -Uvh epel-release-6*.rpm
 
-wget http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
-sudo rpm -Uvh rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
+#wget http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
+#sudo rpm -Uvh rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 
-sudo yum install -y 
+#sudo yum install -y 
 
 mkdir -p "${TOPLEVEL}/SOURCES"
 #wget http://www.imagemagick.org/download/releases/ImageMagick-6.9.1-9.tar.bz2
+#wget http://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/releases/ImageMagick-6.9.1-9.tar.bz2
 wget http://www.imagemagick.org/download/releases/ImageMagick-6.9.2-4.tar.bz2
 
-cp ImageMagick* ${TOPLEVEL}/SOURCES
+cp ImageMagick-6.9.2-4.tar.bz2 ${TOPLEVEL}/SOURCES
 cp ../specs/ImageMagick.spec ${TOPLEVEL}/SPECS
-cp ../specs/*.patch ${TOPLEVEL}/SPECS
+cp ../specs/*.patch ${TOPLEVEL}/SOURCES
 
 rpmbuild -bs -vv ${TOPLEVEL}/SPECS/ImageMagick.spec
 sudo yum-builddep -v ${TOPLEVEL}/SRPMS/ImageMagick-6.9.2-4.src.rpm 
