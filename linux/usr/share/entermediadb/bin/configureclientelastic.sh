d78975ac4c8bd589f1eaa5dd6a90c2fb0b57503a
Christopher Burkey <cburkey@openedit.org>
2015-11-19 13:14:05 -0500

    Sync up with Camarena

--- linux/usr/share/entermediadb/bin/configureclientelastic.sh
+++ linux/usr/share/entermediadb/bin/configureclientelastic.sh
@@ -8,10 +8,10 @@ WEBAPP="/opt/entermediadb/webapp"
 CATALOG="$WEBAPP/WEB-INF/data/$CLIENT_ID/catalog"
 
 mkdir $WEBAPP/$CLIENT_ID/
-cp -rp /opt/entermediadb/webapp/learningmedia/*  $WEBAPP/$CLIENT_ID/
+cp -rp $WEBAPP/template/*  $WEBAPP/$CLIENT_ID/
 
-mkdir -p $CATALOG
-cp -rp /opt/entermediadb/common/data/* $CATALOG
+#mkdir -p $CATALOG
+#cp -rp /opt/entermediadb/common/data/* $CATALOG
 
 echo 'Copying system users/groups'
 mkdir -p $CATALOG/users
@@ -22,25 +22,26 @@ echo 'Linking base.xml'
 mkdir -p $WEBAPP/$CLIENT_ID/catalog
 ln -s /usr/share/entermediadb/common/catalog/configuration $WEBAPP/$CLIENT_ID/catalog/
 
-mkdir -p $CATALOG/lists/searchtypes/
+mkdir -p $CATALOG/lists/app/
+sed "s:%client_id:$CLIENT_ID:g;s:%client_name:$CLIENT_NAME:g;s:%sub_domain:$SUB_DOMAIN:g" < /usr/share/entermediadb/common/catalog/lists/app/custom.xml >$CATALOG/lists/app/custom.xml
 
+mkdir -p $CATALOG/lists/searchtypes/
 sed "s:%client_id:$CLIENT_ID:g;s:%client_name:$CLIENT_NAME:g;s:%sub_domain:$SUB_DOMAIN:g" < /usr/share/entermediadb/common/catalog/searchtypes/custom.xml >$CATALOG/lists/searchtypes/custom.xml
 
 mkdir -p $CATALOG/lists/catalogsettings/
-
 sed "s:%client_id:$CLIENT_ID:g;s:%client_name:$CLIENT_NAME:g;s:%sub_domain:$SUB_DOMAIN:g" < /usr/share/entermediadb/common/catalog/catalogsettings/custom.xml >$CATALOG/lists/catalogsettings/custom.xml
 
 
 outfile="<page>\n   <inner-layout>/theme/layouts/landing.html</inner-layout>\n  <property name=\"clientid\">$CLIENT_ID</property>\n <property name=\"clientname\">$CLIENT_NAME</property>\n <property name=\"clientdomainprefix\">$CLIENT_DOMAIN</property>\n <property name=\"virtual\">true</property>\n</page>"
-echo -e $outfile > /opt/entermediadb/webapp/$CLIENT_ID/index.xconf
+echo -e $outfile > $WEBAPP/$CLIENT_ID/index.xconf
 
 
 outfile="<page> <property name=\"fallbackdirectory\">/WEB-INF/base/emshare</property> <property name=\"title\">$CLIENT_NAME - EMShare</property> <property name=\"appmodule\">asset</property> <property name=\"themeid\">emsharedefault</property> <property name=\"catalogid\">$CLIENT_ID/catalog</property> <property name=\"applicationid\">$CLIENT_ID/emshare</property></page>"
-echo -e $outfile > /opt/entermediadb/webapp/$CLIENT_ID/emshare/_site.xconf
+echo -e $outfile > $WEBAPP/$CLIENT_ID/emshare/_site.xconf
 
 outfile="<page> <property name=\"fallbackdirectory\">/WEB-INF/base/mediadb</property> <property name=\"title\">$CLIENT_NAME - EMShare</property> <property name=\"appmodule\">asset</property> <property name=\"themeid\">emsharedefault</property> <property name=\"catalogid\">$CLIENT_ID/catalog</property> <property name=\"applicationid\">$CLIENT_ID/emshare</property></page>"
-echo -e $outfile > /opt/entermediadb/webapp/$CLIENT_ID/mediadb/_site.xconf
+echo -e $outfile > $WEBAPP/$CLIENT_ID/mediadb/_site.xconf
 
 outfile="<page><property name=\"fallbackdirectory\">/media/catalog</property><property name=\"catalogid\">$CLIENT_ID/catalog</property></page>"
-echo -e $outfile > /opt/entermediadb/webapp/$CLIENT_ID/catalog/_site.xconf
+echo -e $outfile > $WEBAPP/$CLIENT_ID/catalog/_site.xconf
 
--- linux/usr/share/entermediadb/tomcat/bin/setenv.sh
+++ linux/usr/share/entermediadb/tomcat/bin/setenv.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="-d64 -Xms256m -Xmx2024m -XX:MaxPermSize=128m"
+export JAVA_OPTS="-d64 -Xms256m -Xmx2024m -XX:+UseG1GC -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -Djava.security.egd=file:///dev/urandom"
 export CATALINA_HOME="/usr/share/entermediadb/tomcat"
