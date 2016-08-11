bdab1ee3cf80025a61d381059b5a41064624a7e0
Adam <adam@openedit.org>
2016-08-11 17:16:51 -0400

    update drupal/em docker

--- linux/docker/entermedia/run.sh
+++ linux/docker/entermedia/run.sh
@@ -19,25 +19,27 @@ if [[ ! -d "${ENDPOINT}/webapp" ]]; then
 	sudo mkdir -p ${ENDPOINT}
 	#Copy webapp, data and tomcat
 	sudo cp -rp ${ENTERMEDIA_SHARE}/webapp ${ENDPOINT}/
+	sudo cp -rp ${ENTERMEDIA_SHARE}/conf/ffmpeg ${ENDPOINT}/.ffmpeg
+	sudo cp -rp ${ENTERMEDIA_SHARE}/conf/im/delegates.xml ${ENDPOINT}/
 	sudo mv ${ENDPOINT}/webapp/WEB-INF/data ${ENDPOINT}/data
-	sudo chown -R entermedia. ${ENDPOINT}
 fi
 
 if [[ ! -d "${ENDPOINT}/tomcat${PORT}" ]]; then
 	sudo cp -rp ${ENTERMEDIA_SHARE}/tomcat ${ENDPOINT}/tomcat${PORT}
 	sudo mkdir ${ENDPOINT}/tomcat${PORT}/logs
-	sudo chown -R entermedia. ${ENDPOINT}
-	sed "s/%PORT%/${PORT}/g;s/%NODE_ID%/${NODE_ID}/g" <"${ENTERMEDIA_SHARE}/tomcat/conf/server.xml.cluster" >"${ENDPOINT}/tomcat${PORT}/conf/server.xml"
-	sed "s|%ENDPOINT%|${ENDPOINT}|g" <"${ENTERMEDIA_SHARE}/tomcat/bin/tomcat.template" >"${ENDPOINT}/tomcat${PORT}/bin/tomcat"
+	sudo sh -c 'sed "s/%PORT%/${PORT}/g;s/%NODE_ID%/${NODE_ID}/g" <"${ENTERMEDIA_SHARE}/tomcat/conf/server.xml.cluster" >"${ENDPOINT}/tomcat${PORT}/conf/server.xml"'
+	sudo sh -c 'sed "s|%ENDPOINT%|${ENDPOINT}|g" <"${ENTERMEDIA_SHARE}/tomcat/bin/tomcat.template" >"${ENDPOINT}/tomcat${PORT}/bin/tomcat"'
 	echo Updating node.xml cluster name ...
-	sed "s|%CLUSTER_NAME%|${CLUSTER_NAME}|g" <"${ENTERMEDIA_SHARE}/conf/node.xml.cluster" >"${ENDPOINT}/webapp/WEB-INF/node.xml"
+	sudo sh -c 'sed "s|%CLUSTER_NAME%|${CLUSTER_NAME}|g" <"${ENTERMEDIA_SHARE}/conf/node.xml.cluster" >"${ENDPOINT}/webapp/WEB-INF/node.xml"'
 fi
 
+sudo chown -R entermedia. ${ENDPOINT}
 # Run catalina in image to keep alive
-sudo docker run  -p $PORT:$PORT \
+sudo docker run -it -p $PORT:$PORT \
 	-v ${ENDPOINT}/webapp:/opt/entermediadb/webapp \
 	-v ${ENDPOINT}/data:/opt/entermediadb/webapp/WEB-INF/data \
 	-v ${ENDPOINT}/tomcat${PORT}:/opt/entermediadb/tomcat \
-	-u entermedia \
-	-it clients:${CLIENT} \
+	-v ${ENDPOINT}/.ffmpeg:/home/entermedia/ \
+	-v ${ENDPOINT}/delegates.xml:/etc/ImageMagick-6/delegates.xml \
+	clients:${CLIENT} \
 	/usr/bin/entermediadb-deploy /opt/entermediadb
