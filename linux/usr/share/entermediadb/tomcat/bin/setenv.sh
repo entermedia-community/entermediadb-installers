4cb26d5323936a08cf6b2fcf0521f3186b7d8d10
Christopher Burkey <cburkey@openedit.org>
2016-11-01 09:23:56 -0400

    Dedup

--- linux/usr/share/entermediadb/tomcat/bin/setenv.sh
+++ linux/usr/share/entermediadb/tomcat/bin/setenv.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="-d64 -Xms256m -Xmx3024m -XX:+UseG1GC -Djava.security.egd=file:///dev/urandom"
+export JAVA_OPTS="-d64 -Xms256m -Xmx3024m -XX:+UseG1GC -XX:+UseStringDeduplication -Djava.security.egd=file:///dev/urandom"
 export CATALINA_HOME="/usr/share/entermediadb/tomcat"
