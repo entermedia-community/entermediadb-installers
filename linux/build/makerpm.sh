b11a132822fbf8d7f7f362e9e455acb93361c64e
cristobalmejia <cristobal@entermediasoftware.com>
2018-03-26 15:40:31 -0600

    Fix makerpm paths

--- linux/build/makerpm.sh
+++ linux/build/makerpm.sh
@@ -6,7 +6,7 @@ BRANCH=$3
 INPUT=../$PLATFORM
 VERSION=$(cat ../../VERSION${BRANCH}.md)
 DEPLOY=../../deploy
-REPO=~/workspace/drive/emdev/repo
+REPO=/workspace/drive/emdev/repo
 rm -rf $DEPLOY
 mkdir -p $DEPLOY/SPECS
 
@@ -14,20 +14,15 @@ sed "s/{{RELEASE}}/${RELEASE}/g;s/{{VERSION}}/${VERSION}/g;" $INPUT/Entermedia${
 ./maketar.sh $RELEASE $PLATFORM $BRANCH
 
 ./makesrc.sh Entermedia.spec
-cd ../../deploy
+cd $DEPLOY
 rpmbuild --rebuild SRPMS/entermediadb*  || { echo "rpmbuild failed"; exit 1; }
 
 cp ./RPMS/x86_64/*.rpm $REPO/$PLATFORM/6/x86_64/rpms
 cp ./RPMS/x86_64/*.rpm $REPO/$PLATFORM/7/x86_64/rpms
 
-#yum install createrepo
-
 #this is not done on the build server in manual_rpms_em9dev.sh
+#yum install createrepo
 #createrepo --update  $REPO/$PLATFORM/6/x86_64/rpms
 #createrepo --update  $REPO/$PLATFORM/7/x86_64/rpms
 
-#~/workspace/insync-portable/insync-portable force_sync $REPO/$PLATFORM
-
-##
 ##DELETE OLD RPMS????
-##
