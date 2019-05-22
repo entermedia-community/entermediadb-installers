f2df4e6dfa4b310075ddcf9aacfae5d16629e4dc
Thomas N <t.nadalie33@gmail.com>
2019-05-22 09:32:40 -0500

    Add container support java opt

--- linux/usr/share/entermediadb/tomcat/bin/setenv.sh
+++ linux/usr/share/entermediadb/tomcat/bin/setenv.sh
@@ -1,3 +1,3 @@
-export JAVA_OPTS="-server -Xms256m -Xmx8g -XX:+UseG1GC -XX:MaxGCPauseMillis=500  -XX:+UseStringDeduplication -Djava.security.egd=file:///dev/urandom"
+export JAVA_OPTS="-server -Xms256m -Xmx8g -XX:UseContainerSupport -XX:+UseG1GC -XX:MaxGCPauseMillis=500  -XX:+UseStringDeduplication -Djava.security.egd=file:///dev/urandom"
 
 export CATALINA_HOME="/usr/share/entermediadb/tomcat"
