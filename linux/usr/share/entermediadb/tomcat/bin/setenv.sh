c7ca15cf253b2247a673f25ba918580d6e95b4e7
The Builder <buildserver@openedit.org>
2019-06-06 14:31:24 +0000

    Grow default heap space

--- linux/usr/share/entermediadb/tomcat/bin/setenv.sh
+++ linux/usr/share/entermediadb/tomcat/bin/setenv.sh
@@ -1,3 +1,3 @@
-export JAVA_OPTS="-server -Xms256m -Xmx8g -XX:+UseG1GC -XX:MaxGCPauseMillis=500  -XX:+UseStringDeduplication -Djava.security.egd=file:///dev/urandom"
+export JAVA_OPTS="-server -Xss300k -Xms256m -Xmx8g -XX:+UseG1GC -XX:MaxGCPauseMillis=500  -XX:+UseStringDeduplication -Djava.security.egd=file:///dev/urandom"
 
 export CATALINA_HOME="/usr/share/entermediadb/tomcat"
