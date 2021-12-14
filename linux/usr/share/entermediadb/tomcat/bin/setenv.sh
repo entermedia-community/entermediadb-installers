29efc365278e4af3ac1cfaa56a58f9503e4fc7b8
Cristobal Mejia <cristobal@openedit.org>
2021-12-14 11:52:37 -0600

    Format MSgNoLookups

--- linux/usr/share/entermediadb/tomcat/bin/setenv.sh
+++ linux/usr/share/entermediadb/tomcat/bin/setenv.sh
@@ -1,3 +1,3 @@
-export JAVA_OPTS="-server -Xss300k -Xms256m -Xmx8g -XX:+UseG1GC -XX:MaxGCPauseMillis=500  -XX:+UseStringDeduplication -Djava.security.egd=file:///dev/urandom"
+export JAVA_OPTS="-Dlog4j2.formatMsgNoLookups=true -server -Xss300k -Xms256m -Xmx8g -XX:+UseG1GC -XX:MaxGCPauseMillis=500  -XX:+UseStringDeduplication -Djava.security.egd=file:///dev/urandom"
 
 export CATALINA_HOME="/usr/share/entermediadb/tomcat"
